027d293af32dac21d2f14c31573b991a8fbb8097
Kryobane <user@host.com>
2019-08-20 21:54:57 +1000

    moved create menu into sub folder

--- Assets/SO Architecture/Utility/SOArchitecture_Utility.cs
+++ Assets/SO Architecture/Utility/SOArchitecture_Utility.cs
@@ -6,9 +6,10 @@
         public const int ASSET_MENU_ORDER_EVENTS = 122;
         public const int ASSET_MENU_ORDER_COLLECTIONS = 123;
 
-        public const string VARIABLE_SUBMENU = "Variables/";
-        public const string COLLECTION_SUBMENU = "Collections/";
-        public const string GAME_EVENT = "Game Events/";
+        public const string SOA_SUBMENU = "SOA/";
+        public const string VARIABLE_SUBMENU = SOA_SUBMENU + "Variables/";
+        public const string COLLECTION_SUBMENU = SOA_SUBMENU + "Collections/";
+        public const string GAME_EVENT = SOA_SUBMENU + "Game Events/";
 
         public const string ADVANCED_GAME_EVENT = GAME_EVENT + "Advanced/";
         public const string ADVANCED_VARIABLE_SUBMENU = VARIABLE_SUBMENU + "Advanced/";
